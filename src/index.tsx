e801a400c9c1818a82bf4df3c4a71ba205df7f40
edsoalter <edsolater@qq.com>
2023-12-24 22:11:23 +0800

    🧪 add test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import { mul } from '@edsolater/fnkit'
 
 import './index.css'
 import App from './App'
@@ -7,3 +8,14 @@ import App from './App'
 const root = document.getElementById('root')
 
 render(() => <App />, root!)
+
+console.time('run origin math::multiply')
+for (let i = 0; i < 1000000; i++) {
+  i * i
+}
+console.timeEnd('run origin math::multiply')
+console.time('run fnkit mul')
+for (let i = 0; i < 1000000; i++) {
+  mul(i, i)
+}
+console.timeEnd('run fnkit mul')
